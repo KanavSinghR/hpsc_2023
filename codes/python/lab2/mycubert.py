e68e7afbddd36373feca8ce19b1280ca0c0017c2
Kanav Singh Rana <d22069@students.iitmandi.ac.in>
2023-02-13 11:59:55 +0530

    Adding file

---
+++ codes/python/lab2/mycubert.py
@@ -0,0 +1,10 @@
+x=float(input("Enter the value you want to calculate cube root of: "))
+s=2;
+kmax=100;
+for k in range(kmax):
+	t=s
+	print("At iteration number %s, s= %s" %(k,s))
+	s=(2*s+x/s**2)/3
+	if s==t:
+		break
+print("The cube root of %s is: %s",x,s)
