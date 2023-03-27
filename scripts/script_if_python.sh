35dd0d32b6337d1a5859a272f0087ad4fcdd8fca
KanavSinghR <d22069@students.iitmandi.ac.in>
2023-03-28 01:32:17 +0530

    Script updated

--- scripts/script_if_python.sh
+++ scripts/script_if_python.sh
@@ -11,9 +11,10 @@ echo "Running on: "
 cat ${PBS_NODEFILE}
 cat $PBS_NODEFILE > machines.list
 echo "Program Output begins: "
-if [ $(cat ${PBS_NODEFILE}) == {n121.cluster.iitmandi.ac.in} ]; then
+if [ "$(cat $PBS_NODEFILE)" == $echo "n121.cluster.iitmandi.ac.in" ]; then
 	source ~/virtualenvs/venv/bin/activate
 	python -c 'from mysqrt import sqrtNT; sqrtNT(2,debug=True)'
 else
 	echo "File did not go to n121.cluster.iitmandi.ac.in"
+	echo $(cat $PBS_NODEFILE)
 fi
